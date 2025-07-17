ef1d3154a55df8637b78c746621d9228cd38dbf3
pavani agarwal <agarwalpavani2024@gmail.com>
2025-07-18 02:20:09 +0530

    erghh

---
+++ api/exhangeToken.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { code, code_verifier, redirect_uri } = req.body;
+
+  const params = new URLSearchParams();
+  params.append("client_id", process.env.SPOTIFY_CLIENT_ID);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", redirect_uri);
+  params.append("code_verifier", code_verifier);
+
+  try {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: params,
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      return res.status(400).json(data);
+    }
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error", detail: error.message });
+  }
+}
+
